541d0a37c16ae71885a0fc41756f3972fb7541a0
jsaliba <justin.saliba@ricston.com>
2013-06-18 12:08:35 +0200

    Added GetCalendarList test cases

---
+++ calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/GetCalendarListTestCases.java
@@ -0,0 +1,90 @@
+package org.mule.module.google.calendar.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.module.google.calendar.model.Calendar;
+import org.mule.module.google.calendar.model.CalendarList;
+import org.mule.modules.google.api.client.batch.BatchResponse;
+
+public class GetCalendarListTestCases extends GoogleCalendarTestParent {
+
+	protected List<Calendar> insertedCalendars = new ArrayList<Calendar>();
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (Map<String, Object>) context.getBean("getCalendarList");
+			
+			int numCalendars = Integer.parseInt(testObjects.get("numCalendars").toString());
+			
+			// Create calendar instances
+			List<Calendar> calendars = new ArrayList<Calendar>();
+			for (int i = 0; i < numCalendars; i++) {
+				calendars.add(getCalendar("This is a title"));
+			}
+
+			// Insert calendars and record their IDs
+			BatchResponse<Calendar> response = insertCalendars(calendars);			
+			for (Calendar calendar : response.getSuccessful()) {
+				insertedCalendars.add(calendar);
+			}						
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, SanityTests.class})
+	@Test
+	public void testGetCalendarList() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("get-calendar-list");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			List<CalendarList> calendarList = (List<CalendarList>) response.getMessage().getPayload();
+			
+			for (Calendar insertedCalendar : insertedCalendars) {
+				assertTrue(calendarExistsInList(calendarList, insertedCalendar));
+			}
+			
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			fail();
+		}
+	}
+	
+	private boolean calendarExistsInList(List<CalendarList> list, Calendar toSearch) {
+		for (CalendarList calendar : list) {
+			if (calendar.getId().equals(toSearch.getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			// Delete the calendars
+			deleteCalendars(insertedCalendars);			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
--- calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/GoogleCalendarTestParent.java
+++ calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/GoogleCalendarTestParent.java
@@ -10,17 +10,21 @@
 
 package org.mule.module.google.calendar.automation.testcases;
 
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
+import org.mule.api.MuleEvent;
 import org.mule.api.config.MuleProperties;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.api.store.ObjectStore;
 import org.mule.api.store.ObjectStoreException;
+import org.mule.module.google.calendar.model.Calendar;
 import org.mule.module.google.calendar.oauth.GoogleCalendarConnectorOAuthState;
+import org.mule.modules.google.api.client.batch.BatchResponse;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -55,4 +59,28 @@ public class GoogleCalendarTestParent extends FunctionalTestCase {
 	public static void beforeClass() {
 		context = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILES);
 	}
+	
+	/*
+	 * Helper methods below
+	 */
+	
+	protected BatchResponse<Calendar> insertCalendars(List<Calendar> calendars) throws Exception {
+		testObjects.put("calendarsRef", calendars);
+		MessageProcessor flow = lookupFlowConstruct("batch-insert-calendar");
+		MuleEvent response = flow.process(getTestEvent(testObjects));
+		return (BatchResponse<Calendar>) response.getMessage().getPayload();
+	}
+	
+	protected void deleteCalendars(List<Calendar> calendars) throws Exception {
+		testObjects.put("calendarsRef", calendars);
+		MessageProcessor flow = lookupFlowConstruct("batch-delete-calendar");
+		MuleEvent response = flow.process(getTestEvent(testObjects));
+	}
+	
+	protected Calendar getCalendar(String summary) {
+		Calendar calendar = new Calendar();
+		calendar.setSummary(summary);
+		return calendar;
+	}
+	
 }
