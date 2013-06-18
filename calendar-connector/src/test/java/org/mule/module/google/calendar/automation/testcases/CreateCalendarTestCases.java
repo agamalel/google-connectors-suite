35fb908fd2d465f0caf5bab4814ae2b7fbdb8152
jsaliba <justin.saliba@ricston.com>
2013-06-18 09:24:43 +0200

    First commit

---
+++ calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/CreateCalendarTestCases.java
@@ -0,0 +1,79 @@
+/**
+ * Mule Google Calendars Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.google.calendar.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.module.google.calendar.model.Calendar;
+
+public class CreateCalendarTestCases extends GoogleCalendarTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		testObjects = (Map<String, Object>) context.getBean("createCalendar");
+	}
+	
+	
+	@Test
+	public void testCreateCalendar() {
+		try {
+			
+			Calendar originalCalendar = (Calendar) testObjects.get("calendarRef");
+			
+			// Create the calendar
+			MessageProcessor flow = lookupFlowConstruct("create-calendar");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			// Assertions on the calendar
+			Calendar createdCalendar = (Calendar) response.getMessage().getPayload();
+			assertTrue(createdCalendar != null);
+			assertTrue(createdCalendar.getSummary().equals(originalCalendar.getSummary()));
+
+			// Store the ID in testObjects 
+			testObjects.put("id", createdCalendar.getId());
+			
+			flow = lookupFlowConstruct("get-calendar-by-id");
+			response = flow.process(getTestEvent(testObjects));
+
+			// Assertions on equality
+			Calendar returnedCalendar = (Calendar) response.getMessage().getPayload();
+			assertTrue(returnedCalendar != null);
+			assertTrue(returnedCalendar.getId().equals(createdCalendar.getId()));
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			// Delete the calendar
+			MessageProcessor flow = lookupFlowConstruct("delete-calendar");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+}
---
+++ calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/GoogleCalendarTestParent.java
@@ -0,0 +1,58 @@
+/**
+ * Mule Google Calendars Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.google.calendar.automation.testcases;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+import org.mule.api.config.MuleProperties;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.api.store.ObjectStore;
+import org.mule.api.store.ObjectStoreException;
+import org.mule.module.google.calendar.oauth.GoogleCalendarConnectorOAuthState;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class GoogleCalendarTestParent extends FunctionalTestCase {
+
+	// Set global timeout of tests to 10minutes
+    @Rule
+    public Timeout globalTimeout = new Timeout(600000);
+    
+	protected static final String[] SPRING_CONFIG_FILES = new String[] { "AutomationSpringBeans.xml" };
+	protected static ApplicationContext context;
+	protected Map<String, Object> testObjects;
+
+	@Override
+	protected String getConfigResources() {
+		return "automation-test-flows.xml";
+	}
+
+	protected MessageProcessor lookupFlowConstruct(String name) {
+		return (MessageProcessor) muleContext.getRegistry()
+				.lookupFlowConstruct(name);
+	}
+	
+	@Before
+	public void init() throws ObjectStoreException {
+		ObjectStore objectStore = muleContext.getRegistry().lookupObject(MuleProperties.DEFAULT_USER_OBJECT_STORE_NAME);
+		objectStore.store("accessTokenId", (GoogleCalendarConnectorOAuthState)context.getBean("connectorOAuthState"));
+	}
+
+	@BeforeClass
+	public static void beforeClass() {
+		context = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILES);
+	}
+}
