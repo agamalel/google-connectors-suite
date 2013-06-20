92dc1f1acbd3dc9a2afbbee7b5a568dd046b2022
clarecini <clare.cini@ricston.com>
2013-06-20 14:48:26 +0200

    Added get-acl-rule-by-id test cases

---
+++ calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/GetAclRuleByIdTestCases.java
@@ -0,0 +1,83 @@
+package org.mule.module.google.calendar.automation.testcases;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.module.google.calendar.model.AclRule;
+import org.mule.module.google.calendar.model.Calendar;
+
+public class GetAclRuleByIdTestCases extends GoogleCalendarTestParent {
+	
+	
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (Map<String, Object>) context.getBean("insertAclRule");
+			
+			// Insert calendar and get reference to retrieved calendar
+			Calendar calendar = insertCalendar((Calendar) testObjects.get("calendarRef"));
+			
+			// Replace old calendar instance with new instance
+			testObjects.put("calendarRef", calendar);
+			testObjects.put("calendarId", calendar.getId());
+			
+			
+			// Insert the ACL rule
+			MessageProcessor flow = lookupFlowConstruct("insert-acl-rule");
+			MuleEvent event = flow.process(getTestEvent(testObjects));
+						
+			AclRule returnedAclRule = (AclRule) event.getMessage().getPayload();
+			testObjects.put("aclRule", returnedAclRule);	
+			testObjects.put("ruleId", returnedAclRule.getId());
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, SanityTests.class})	
+	@Test
+	public void testGetAclRuleById() {
+		try {
+			String ruleIdBefore = testObjects.get("ruleId").toString();
+			
+			MessageProcessor flow = lookupFlowConstruct("get-acl-rule-by-id");
+			MuleEvent event = flow.process(getTestEvent(testObjects));
+			
+			AclRule afterProc = (AclRule) event.getMessage().getPayload();
+			String ruleIdAfter = afterProc.getId();
+			
+			assertEquals(ruleIdBefore,ruleIdAfter);		
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	
+	@After
+	public void tearDown() {
+		try {
+			String calendarId = testObjects.get("calendarId").toString();
+			deleteCalendar(calendarId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+}
