88a7609ed13a2b01ab6661f79d7b6b67611389c4
Justin Calleja <calleja.justin@gmail.com>
2013-06-24 17:24:31 +0200

    Added GoogleTaskTestParent and InsertTaskListTestCases with the files it needs to run the test.

---
+++ tasks-connector/src/test/java/org/mule/module/google/task/automation/testcases/GoogleTaskTestParent.java
@@ -0,0 +1,53 @@
+package org.mule.module.google.task.automation.testcases;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+import org.mule.api.config.MuleProperties;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.api.store.ObjectStore;
+import org.mule.api.store.ObjectStoreException;
+import org.mule.module.google.task.oauth.GoogleTasksConnectorOAuthState;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class GoogleTaskTestParent extends FunctionalTestCase {
+	
+	// Set global timeout of tests to 10minutes
+    @Rule
+    public Timeout globalTimeout = new Timeout(600000);
+    
+	protected static final String[] SPRING_CONFIG_FILES = new String[] { "AutomationSpringBeans.xml" };
+	protected static ApplicationContext context;
+	protected Map<String, Object> testObjects;
+
+	@Before
+	public void init() throws ObjectStoreException {
+		ObjectStore objectStore = muleContext.getRegistry().lookupObject(MuleProperties.DEFAULT_USER_OBJECT_STORE_NAME);
+		objectStore.store("accessTokenId", (GoogleTasksConnectorOAuthState)context.getBean("connectorOAuthState"));
+	}
+	
+	@Override
+	protected String getConfigResources() {
+		return "automation-test-flows.xml";
+	}
+
+	protected MessageProcessor lookupFlowConstruct(String name) {
+		return (MessageProcessor) muleContext.getRegistry()
+				.lookupFlowConstruct(name);
+	}
+
+	@BeforeClass
+	public static void beforeClass() {
+		context = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILES);
+	}
+	
+	protected void setTestObjects(Map<String, Object> testObjects) {
+		this.testObjects = testObjects;
+	}
+	
+}
---
+++ tasks-connector/src/test/java/org/mule/module/google/task/automation/testcases/InsertTaskListTestCases.java
@@ -0,0 +1,73 @@
+package org.mule.module.google.task.automation.testcases;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.module.google.task.model.TaskList;
+
+public class InsertTaskListTestCases extends GoogleTaskTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("insertTaskList");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			lookupFlowConstruct("delete-task-list").process(getTestEvent(testObjects));
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testInsertTaskList() {
+		testObjects = (HashMap<String, Object>) context.getBean("insertTaskList");
+
+		MuleEvent insertTaskListResponse = null;
+		try {
+			insertTaskListResponse = lookupFlowConstruct("insert-task-list").process(getTestEvent(testObjects));
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			fail();
+		}
+		
+		assertTrue("The object returned by insert-task-list should be of type TaskList", 
+				insertTaskListResponse.getMessage().getPayload() instanceof TaskList);
+		assertEquals("The title of the TaskList returned by insert-task-list should be equal to the title of the TaskList inserted",
+				((TaskList) testObjects.get("taskListRef")).getTitle(),
+				((TaskList) insertTaskListResponse.getMessage().getPayload()).getTitle());
+		
+		// add taskListId of the inserted list to query for the list (and to delete it in tearDown)
+		testObjects.put("taskListId", ((TaskList) insertTaskListResponse.getMessage().getPayload()).getId());
+
+		MuleEvent getTaskListByIdResponse = null;
+		try{
+			getTaskListByIdResponse = lookupFlowConstruct("get-task-list-by-id").process(getTestEvent(testObjects));
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertTrue("The object returned by get-task-list-by-id should be of type TaskList", 
+				getTaskListByIdResponse.getMessage().getPayload() instanceof TaskList);
+		assertEquals("The title of the TaskList returned by get-task-list-by-id should be equal to the title of the TaskList inserted",
+				((TaskList) testObjects.get("taskListRef")).getTitle(),
+				((TaskList) getTaskListByIdResponse.getMessage().getPayload()).getTitle());
+	}
+}
