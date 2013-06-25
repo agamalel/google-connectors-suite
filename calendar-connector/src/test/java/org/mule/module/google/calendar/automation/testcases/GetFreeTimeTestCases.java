eb3a453a5cd830fb320c9612c7aa2cabd1ff8017
jsaliba <justin.saliba@ricston.com>
2013-06-25 13:28:29 +0200

    Updated get free time test cases

--- calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/GetFreeTimeTestCases.java
+++ calendar-connector/src/test/java/org/mule/module/google/calendar/automation/testcases/GetFreeTimeTestCases.java
@@ -1,5 +1,6 @@
 package org.mule.module.google.calendar.automation.testcases;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.List;
@@ -55,16 +56,19 @@ public class GetFreeTimeTestCases extends GoogleCalendarTestParent {
 			
 			testObjects.put("timeMin", timeMin);
 			testObjects.put("timeMax", timeMax);
-			
+
 			MessageProcessor flow = lookupFlowConstruct("get-free-time");
 			MuleEvent response = flow.process(getTestEvent(testObjects));
 			
 			FreeBusy freeBusy =(FreeBusy) response.getMessage().getPayload();
-			
+								
 			// We should only be working with the calendar created specifically for this test
 			FreeBusyCalendar freeBusyCalendar = freeBusy.getCalendars().get(calendarId);
 			
 			List<TimePeriod> busyTimePeriods = freeBusyCalendar.getBusy();
+			assertTrue(busyTimePeriods.size() == 1);
+			
+			TimePeriod busyTimePeriod = busyTimePeriods.get(0);
 			
 			System.out.println(ToStringBuilder.reflectionToString(busyTimePeriods));
 			
