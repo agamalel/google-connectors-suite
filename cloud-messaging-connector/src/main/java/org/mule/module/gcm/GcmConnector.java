5c55019bfc16aeff824cfdcc2b4bb4db4b75d6a1
David Dossot <david@dossot.net>
2013-07-11 13:21:10 -0700

    added notification key creation

--- cloud-messaging-connector/src/main/java/org/mule/module/gcm/GcmConnector.java
+++ cloud-messaging-connector/src/main/java/org/mule/module/gcm/GcmConnector.java
@@ -7,6 +7,7 @@
 
 package org.mule.module.gcm;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
@@ -14,10 +15,15 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+import org.mule.api.DefaultMuleException;
 import org.mule.api.MessagingException;
 import org.mule.api.MuleContext;
 import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
 import org.mule.api.annotations.Configurable;
 import org.mule.api.annotations.Module;
@@ -29,11 +35,16 @@ import org.mule.config.i18n.MessageFactory;
 import org.mule.module.gcm.model.Data;
 import org.mule.module.gcm.model.GcmRequest;
 import org.mule.module.gcm.model.GcmResponse;
+import org.mule.module.gcm.model.NotificationRequest;
+import org.mule.module.gcm.model.NotificationRequest.Operation;
+import org.mule.module.gcm.model.NotificationResponse;
 import org.mule.transformer.types.MimeTypes;
 import org.mule.transport.http.HttpConnector;
 import org.mule.transport.http.HttpConstants;
+import org.mule.util.IOUtils;
 import org.mule.util.MapUtils;
 import org.mule.util.NumberUtils;
+import org.mule.util.StringUtils;
 
 /**
  * Mule connector for Google Cloud Messaging (GCM / CCS).
@@ -50,11 +61,26 @@ public class GcmConnector implements MuleContextAware
     public static final String GCM_SEND_URI = "https://android.googleapis.com/gcm/send";
     public static final String GCM_NOTIFICATION_URI = "https://android.googleapis.com/gcm/notification";
 
+    private static final TypeReference<GcmResponse> GCM_RESPONSE_TYPE_REFERENCE = new TypeReference<GcmResponse>()
+    {
+        // NOOP
+    };
+    private static final TypeReference<NotificationResponse> NOTIFICATION_RESPONSE_TYPE_REFERENCE = new TypeReference<NotificationResponse>()
+    {
+        // NOOP
+    };
+
     /**
      * The Google APIs key.
      */
     @Configurable
     private String apiKey;
+
+    /**
+     * The project ID.
+     */
+    @Configurable
+    private String projectId;
 
     /**
      * The connector to use to reach Neo4j: configure only if there is more than one HTTP/HTTPS
@@ -65,6 +91,7 @@ public class GcmConnector implements MuleContextAware
     private org.mule.api.transport.Connector connector;
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final Log LOGGER = LogFactory.getLog(GcmConnector.class);
 
     private MuleContext muleContext;
 
@@ -122,15 +149,87 @@ public class GcmConnector implements MuleContextAware
             gcmRequest.setData(gcmData);
         }
 
-        final String gcmRequestJson = OBJECT_MAPPER.writeValueAsString(gcmRequest);
-
         final String uri = GCM_SEND_URI + (connector != null ? "?connector=" + connector.getName() : "");
+        return httpPostJson(gcmRequest, uri, GCM_RESPONSE_TYPE_REFERENCE, muleEvent);
+    }
+
+    @Processor
+    @Inject
+    public NotificationResponse createNotificationKey(final String notificationKeyName,
+                                                      final List<String> registrationIds,
+                                                      final MuleEvent muleEvent) throws Exception
+    {
+        final NotificationRequest notificationRequest = new NotificationRequest();
+        notificationRequest.setOperation(Operation.CREATE);
+        notificationRequest.setNotificationKeyName(notificationKeyName);
+        notificationRequest.getRegistrationIds().addAll(registrationIds);
+
+        return httpPostJson(notificationRequest, GCM_NOTIFICATION_URI, NOTIFICATION_RESPONSE_TYPE_REFERENCE,
+            muleEvent);
+    }
+
+    // TODO addNotificationRegistrations
+    // TODO removeNotificationRegistrations
+
+    private String serializeEntityToJson(final Object entity) throws MuleException
+    {
+        if (entity == null)
+        {
+            return null;
+        }
+
+        try
+        {
+            return OBJECT_MAPPER.writeValueAsString(entity);
+        }
+        catch (final IOException ioe)
+        {
+            throw new DefaultMuleException("Failed to serialize to JSON: " + entity, ioe);
+        }
+    }
+
+    private <T> T deserializeJsonToEntity(final TypeReference<T> responseType, final MuleMessage response)
+        throws MuleException
+    {
+        try
+        {
+            T entity;
+
+            if (LOGGER.isDebugEnabled())
+            {
+                response.setPayload(IOUtils.toByteArray((InputStream) response.getPayload()));
+                entity = OBJECT_MAPPER.<T> readValue((byte[]) response.getPayload(), responseType);
+            }
+            else
+            {
+                entity = OBJECT_MAPPER.<T> readValue((InputStream) response.getPayload(), responseType);
+            }
+
+            return entity;
+        }
+        catch (final IOException ioe)
+        {
+            throw new DefaultMuleException("Failed to deserialize to: " + responseType.getType() + " from: "
+                                           + renderMessageAsString(response), ioe);
+        }
+    }
+
+    private <T> T httpPostJson(final Object requestEntity,
+                               final String uri,
+                               final TypeReference<T> responseTypeReference,
+                               final MuleEvent muleEvent) throws MuleException, MessagingException, Exception
+    {
+        final String requestJson = serializeEntityToJson(requestEntity);
 
         final Map<String, Object> requestProperties = new HashMap<String, Object>();
         requestProperties.put(HttpConstants.HEADER_CONTENT_TYPE, MimeTypes.JSON);
         requestProperties.put(HttpConstants.HEADER_AUTHORIZATION, "key=" + apiKey);
+        if (StringUtils.isNotBlank(projectId))
+        {
+            requestProperties.put("project_id", projectId);
+        }
 
-        final MuleMessage response = muleContext.getClient().send(uri, gcmRequestJson, requestProperties,
+        final MuleMessage response = muleContext.getClient().send(uri, requestJson, requestProperties,
             muleEvent.getTimeout());
 
         final Object responseStatusCode = response.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY);
@@ -143,7 +242,19 @@ public class GcmConnector implements MuleContextAware
                 muleEvent);
         }
 
-        return OBJECT_MAPPER.readValue((InputStream) response.getPayload(), GcmResponse.class);
+        return deserializeJsonToEntity(responseTypeReference, response);
+    }
+
+    private static String renderMessageAsString(final MuleMessage message)
+    {
+        try
+        {
+            return message.getPayloadAsString();
+        }
+        catch (final Exception e)
+        {
+            return message.toString();
+        }
     }
 
     @Override
@@ -160,6 +271,16 @@ public class GcmConnector implements MuleContextAware
     public void setApiKey(final String apiKey)
     {
         this.apiKey = apiKey;
+    }
+
+    public String getProjectId()
+    {
+        return projectId;
+    }
+
+    public void setProjectId(final String projectId)
+    {
+        this.projectId = projectId;
     }
 
     public org.mule.api.transport.Connector getConnector()
