5920c118d63d1679d25b39ceced3323bafa89a89
jsaliba <justin.saliba@ricston.com>
2013-09-03 13:30:50 +0200

    Updated GetAllSpreadsheets test cases

--- spreadsheets-connector/src/test/java/org/mule/module/google/spreadsheet/automation/testcases/GetAllSpreadsheets.java
+++ spreadsheets-connector/src/test/java/org/mule/module/google/spreadsheet/automation/testcases/GetAllSpreadsheets.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -20,8 +21,11 @@ public class GetAllSpreadsheets extends GoogleSpreadsheetsTestParent {
 	public void setUp() {
 		try {
 			testObjects = (Map<String, Object>) context.getBean("getAllSpreadsheets");
-			String title = (String) testObjects.get("title");
-			createSpreadsheet(title);
+			
+			List<String> spreadsheetTitles = (List<String>) testObjects.get("spreadsheets");
+			for (String spreadsheetTitle : spreadsheetTitles) {
+				createSpreadsheet(spreadsheetTitle);
+			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -33,19 +37,37 @@ public class GetAllSpreadsheets extends GoogleSpreadsheetsTestParent {
 	@Test
 	public void testGetAllSpreadsheets() {
 		try {
-			String title = (String) testObjects.get("title");
+			List<String> createdSpreadsheets = (List<String>) testObjects.get("spreadsheets");
 
 			MessageProcessor flow = lookupFlowConstruct("get-all-spreadsheets");
 			MuleEvent response = flow.process(getTestEvent(testObjects));
 			List<Spreadsheet> spreadsheets = (List<Spreadsheet>) response.getMessage().getPayload();
 
-			boolean found = false;
-			for (Spreadsheet spreadsheet : spreadsheets) {
-				if (spreadsheet.getTitle().equals(title)) {
-					found = true;
+			for (String spreadsheetTitle : createdSpreadsheets) {
+				boolean found = false;
+				for (Spreadsheet spreadsheet : spreadsheets) {
+					if (spreadsheet.getTitle().equals(spreadsheetTitle)) {
+						found = true;
+						break;
+					}
 				}
+				assertTrue(found);
 			}
-			assertTrue(found);
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@After
+	public void tearDown() {
+		try {
+			List<String> spreadsheets = (List<String>) testObjects.get("spreadsheets");
+			for (String spreadsheet : spreadsheets) {
+				deleteSpreadsheet(spreadsheet);
+			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
